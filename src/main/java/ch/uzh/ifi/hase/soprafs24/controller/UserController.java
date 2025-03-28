4afe0a58d296e41f898b347c1c43d7f5fbd6d19e
Mathis Beeler <mathis.beeler@uzh.ch>
2025-03-28 14:47:29 +0100

    fixed tests for US 1

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
@@ -3,7 +3,6 @@ package ch.uzh.ifi.hase.soprafs24.controller;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.UserLoginDTO;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.UserLogoutDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.UserService;
