fe590c94712b3eb3dc5e3c24d74100680ec2788b
Mathis Beeler <mathis.beeler@uzh.ch>
2025-04-25 21:01:53 +0200

    fix api key bug

--- src/main/java/ch/uzh/ifi/hase/soprafs24/api/apiToken.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/api/apiToken.java
@@ -2,31 +2,42 @@ package ch.uzh.ifi.hase.soprafs24.api;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Properties;
+
+import com.google.cloud.secretmanager.v1.AccessSecretVersionRequest;
+import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.nio.file.Paths;
-
 public class apiToken {
-    public static String getApiToken() {        
+
+    public static String getApiToken() {
+        //getting from local.properties 
         try {
             Properties properties = new Properties();
-            String propertiesPath = Paths.get(System.getProperty("user.dir"), "local.properties").toString();
-            properties.load(new FileInputStream(propertiesPath));
+            String path = Paths.get(System.getProperty("user.dir"), "local.properties").toString();
+            properties.load(new FileInputStream(path));
             String apiKey = properties.getProperty("API_KEY");
             if (apiKey != null && !apiKey.isEmpty()) {
                 return apiKey;
             }
-        } catch (IOException e) {
+        } catch (IOException ignored) {
+            //missing file 
         }
-        
-        // Fall back to environment variable
-        try {
-            return System.getenv("API_KEY");
+
+        //getting from secret manager
+        try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+            String secretName = "projects/1075428158089/secrets/API_KEY/versions/latest";
+            AccessSecretVersionRequest request = AccessSecretVersionRequest.newBuilder()
+                    .setName(secretName)
+                    .build();
+            AccessSecretVersionResponse response = client.accessSecretVersion(request);
+            return response.getPayload().getData().toStringUtf8();
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to load api key");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to load API key from Secret Manager", e);
         }
     }
 }
