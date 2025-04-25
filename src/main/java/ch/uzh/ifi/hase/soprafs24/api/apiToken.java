4317b1e3b68c05e1cf458c9ed369fa52f15601ea
Silvan Wyss <silvansascha.wyss@uzh.ch>
2025-04-25 19:38:21 +0200

    partial fix api_key

--- src/main/java/ch/uzh/ifi/hase/soprafs24/api/apiToken.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/api/apiToken.java
@@ -4,13 +4,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 import java.nio.file.Paths;
 
 public class apiToken {
-    public static String getApiToken() {        
+    public static String getLocalApiToken() {        
         try {
             Properties properties = new Properties();
             String propertiesPath = Paths.get(System.getProperty("user.dir"), "local.properties").toString();
@@ -21,12 +18,6 @@ public class apiToken {
             }
         } catch (IOException e) {
         }
-        
-        // Fall back to environment variable
-        try {
-            return System.getenv("API_KEY");
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to load api key");
-        }
+        return null;
     }
 }
